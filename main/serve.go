2c5d5994356a4a78112f2658954aa89fa305672d
Mykola Gurov <ngurov@gmail.com>
2017-09-05 22:03:30 +0200

    simple serving

--- main/serve.go
+++ main/serve.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"net/url"
+	"flag"
+	"net/http/httputil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, world")
+	apiURLStr := flag.String("api", "http://localhost:9200", "the url to proxy API from")
+	flag.Parse()
+
+	apiURL, err := url.Parse(*apiURLStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rProxy := httputil.NewSingleHostReverseProxy(apiURL)
+	http.Handle("/api/", http.StripPrefix("/api", rProxy))
+	http.Handle("/ui/", http.StripPrefix("/ui", http.FileServer(http.Dir("./ui"))))
+	http.Handle("/", http.RedirectHandler("/ui/", http.StatusMovedPermanently))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
