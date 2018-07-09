909e97cfa8084728327fce19ce76c930babe3f18
Mykola Gurov <ngurov@gmail.com>
2018-07-09 21:53:59 +0200

    something has changed here

--- main/serve.go
+++ main/serve.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"net/url"
 	"flag"
-	"net/http/httputil"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
 )
 
-var uiVersion string
+var uiVersion = strings.TrimSpace((string)(MustAsset("ui/REACT_APP_VERSION")))
 
 func main() {
-	apiURLStr := flag.String("api", "http://localhost:9200", "the url to proxy API from")
+	apiURLStr := flag.String("api", "", "the url to proxy API from")
 	portStr := flag.String("port", "8080", "port")
 	flag.Parse()
 
@@ -24,8 +25,9 @@ func main() {
 		r.Header.Set("Kibanator-UI-Version", uiVersion)
 		return nil
 	}
-	http.Handle("/", rProxy)
+	http.Handle("/api/", rProxy)
 	http.Handle("/ui/", http.StripPrefix("/ui", http.FileServer(assetFS())))
 	//http.Handle("/", http.RedirectHandler("/ui/", http.StatusMovedPermanently))
-	log.Fatal(http.ListenAndServe(":" + *portStr, nil))
+	log.Println("ready at port", *portStr)
+	log.Fatal(http.ListenAndServe(":"+*portStr, nil))
 }
